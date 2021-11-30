ccc1c4185dd2637d68f41e5726166d1337c7f9c2
Yarolav Gaponov <yarolav.gaponov@gmail.com>
2021-11-30 14:29:23 +0100

    init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"./cpuminer"
+)
+
+type Block struct {
+	Hash          string
+	Version       uint32
+	PrevBlockHash string
+	MerkleRoot    string
+	Time          uint32
+	Bits          uint32
+	Nonce         uint32
+}
+
+func progress() func(start, end, current uint32) {
+	s := time.Now()
+	var percent int = 0
+	return func(start, end, current uint32) {
+		percent2 := int(float64(current-start) / float64(end-start) * 100)
+		if percent2 > percent {
+			percent = percent2
+			speed := uint32(float64(current-start) / time.Since(s).Seconds())
+			indicator := "[" + strings.Repeat("=", percent) + ">" + strings.Repeat(" ", 100-percent) + "]"
+			fmt.Printf("\rProgress %s %d %% | %d ops/sec", indicator, percent, speed)
+		}
+	}
+}
+
+func main() {
+
+	block := Block{
+		Hash:          "00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d",
+		Version:       1,
+		PrevBlockHash: "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
+		MerkleRoot:    "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
+		Time:          1305998791,
+		Bits:          440711666,
+		Nonce:         2504433986,
+	}
+
+	fmt.Println("Starting mining...")
+
+	var start, end uint32 = 2400000000, 2600000000
+	fmt.Printf("Mining nonce from %d to %d\n", start, end)
+
+	if hash, err := cpuminer.Mine(progress(), block.Version, block.PrevBlockHash, block.MerkleRoot, block.Time, block.Bits, start, end); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("\nFound hash is ", hash)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+CPU Miner
+=========
+Bitcoin CPU miner written in Go.
+
+# Introduction
+
+This is a CPU miner written in Go. It is a proof of concept and is not intended for production use.
+
+# Demo
+
+```bash
+go build -o cpuminer *.go
+./cpuminer
+```
+
+```output
+Starting mining...
+Mining nonce from 2400000000 to 2600000000
+Progress [====================================================>                                                ] 52 % | 859898 ops/sec
+Found hash is  00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d
+```
