e0f939beca5dd37a6ab5e5cda629228645af6181
Yaroslav Gaponov <yaroslav.gaponov@avid.com>
2021-12-16 12:50:47 +0000

    chore: cli with parameters

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"math"
 
 	"github.com/YaroslavGaponov/cpuminer/internal/miner"
 	"github.com/YaroslavGaponov/cpuminer/pkg/bitcoin"
@@ -9,18 +13,26 @@ import (
 
 func main() {
 
-	block := bitcoin.Block{
-		Hash:          "00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d",
-		Version:       1,
-		PrevBlockHash: "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
-		MerkleRoot:    "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
-		Time:          1305998791,
-		Bits:          440711666,
-		Nonce:         2504433986,
+	fileName := flag.String("file", "", "file with block")
+	from := flag.Uint("from", 0, "nonce from")
+	to := flag.Uint("to", math.MaxUint32, "nonce to")
+
+	flag.Parse()
+
+	file, err := ioutil.ReadFile(*fileName)
+	if err != nil {
+		panic(err)
 	}
 
+	var block bitcoin.Block
+	if err := json.Unmarshal(file, &block); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Nonce from %d to %d\n", *from, *to)
+
 	m := miner.New(block)
-	if nonce, err := m.Mine(2400000000, 2600000000); err != nil {
+	if nonce, err := m.Mine(uint32(*from), uint32(*to)); err != nil {
 		fmt.Printf("Error %v", err)
 	} else {
 		fmt.Printf("Nonce is %d", nonce)
--- readme.md
+++ readme.md
@@ -4,28 +4,24 @@ CPU bitcoin miner
 
 # Demo
 
-## Demo block
+## Demo block in `example.json`
 
 ```json
 {
-    "Version":       1,
-    "PrevBlockHash": "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
-    "MerkleRoot":    "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
-    "Time":          1305998791,
-    "Bits":          440711666
+    "hash": "00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d",
+    "ver": 1,
+    "prev_hash": "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
+    "mrkl_root": "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
+    "time": 1305998791,
+    "bits": 440711666,
+    "nonce": 2504433986
 }
-```
-
-## Build
-
-```sh
-go build -o miner cmd/miner/main.go
 ```
 
 ## Run
 
 ```sh
-miner
+go run cmd/miner/main.go --file=example.json --from=2400000000 --to=2600000000
 ```
 
 
